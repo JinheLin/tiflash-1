5453013805b796ef889034ce382e1cc7d4ef25ad
jinhelin <linjinhe33@gmail.com>
2024-01-31 17:36:59 +0800

    ci

--- dbms/src/Storages/DeltaMerge/tests/gtest_interval_tree.cpp
+++ dbms/src/Storages/DeltaMerge/tests/gtest_interval_tree.cpp
@@ -38,8 +38,10 @@ TEST(IntervalTree_test, Find)
     
     auto v1 = tree.find({1, 10});
     ASSERT_TRUE(v1.has_value());
+    ASSERT_EQ(*v1, 110);
     auto v2 = tree.find({20, 30});
     ASSERT_TRUE(v2.has_value());
+    ASSERT_EQ(*v2, 2030);
     auto v3 = tree.find({5, 15});
     ASSERT_FALSE(v3.has_value());
 }
