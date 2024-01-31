a1d0ec458c5b5ce4209866a874af6d55e3ddd408
jinhelin <linjinhe33@gmail.com>
2024-01-31 18:01:40 +0800

    ci

--- dbms/src/Storages/DeltaMerge/tests/gtest_interval_tree.cpp
+++ dbms/src/Storages/DeltaMerge/tests/gtest_interval_tree.cpp
@@ -7,26 +7,17 @@ namespace DB::DM::tests
 TEST(IntervalTree_test, FindOverlap)
 {
     IntervalTree<int, int> tree;
+    // [1, 10), [10, 20), [20, 30)
     tree.insert({1, 10});
+    tree.insert({10, 20});
     tree.insert({20, 30});
-    {
-        IntervalTree<int, int>::Intervals overlaps;
-        tree.findOverlappingIntervals({10, 20}, overlaps, true);
-        for (const auto & o : overlaps)
-        {
-            std::cout << o << std::endl;
-        }
-        std::cout << std::endl;
-    }
 
     {
         IntervalTree<int, int>::Intervals overlaps;
+        // Find overlap with [10, 20)
         tree.findOverlappingIntervals({10, 20}, overlaps, false);
-        for (const auto & o : overlaps)
-        {
-            std::cout << o << std::endl;
-        }
-        std::cout << std::endl;
+        ASSERT_EQ(overlaps.size(), 1);
+        ASSERT_EQ(overlaps.front(), (IntervalTree<int, int>::Interval{10, 20}));
     }
 }
 
@@ -35,7 +26,7 @@ TEST(IntervalTree_test, Find)
     IntervalTree<int, int> tree;
     tree.insert({1, 10, 110});
     tree.insert({20, 30, 2030});
-    
+
     auto v1 = tree.find({1, 10});
     ASSERT_TRUE(v1.has_value());
     ASSERT_EQ(*v1, 110);
